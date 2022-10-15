bf5cb9ddbef4a2220c83c72402eb4e46a419b06f
cyrilmuchemi <cyrilmuchemi@gmail.com>
2022-10-15 10:17:20 +0300

    Complete the hero section

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -3,6 +3,7 @@ import { Cursor, useTypewriter } from "react-simple-typewriter";
 import BackgroundCircles from "./BackgroundCircles";
 import { useLottie } from "lottie-react";
 import computerAnimation from "../computer.json";
+import Link from "next/link";
 
 type Props = {}
 
@@ -31,16 +32,25 @@ export default function Hero({}: Props ) {
         <div className="h-screen flex flex-col space-y-8 items-center justify-center text-center overflow-hidden">
             <BackgroundCircles/>
             <div className="w-[150px] h-[150px]">{View}</div>
-            <div>
+            <div className="z-20">
             <h2 className="text-sm uppercase text-white pb-5 tracking-[15px]">Software Engineer</h2>
             <h1 className="text-3xl lg:text-5xl font-semibold px-10">
                 <span className="text-white">{text}</span>
                 <Cursor cursorColor="#088F8F"/>
             </h1>
-            <div>
+            <div className='pt-5'>
+                <Link href='#about'>
                 <button className="HeroButton">About</button>
+                </Link>
+                <Link href='#skills'>
                 <button className="HeroButton">Skills</button>
+                </Link>
+                <Link href='#project'>
                 <button className="HeroButton">Projects</button>
+                </Link>
+                <Link href='#contact'>
+                <button className="HeroButton">Contact</button>
+                </Link>
             </div>
             </div>
         </div>
