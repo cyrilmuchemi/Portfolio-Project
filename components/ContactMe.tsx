737e966767400a999e9cc9ff263fc507c024f116
cyrilmuchemi <cyrilmuchemi@gmail.com>
2022-11-01 10:43:26 +0300

    Make changes on contact section

--- components/ContactMe.tsx
+++ components/ContactMe.tsx
@@ -17,9 +17,9 @@ export default function ContactMe() {
         window.location.href = `mailto:cyrilmuchemi@gmail?subject=${formData.subject}&body=Hi, my name is ${formData.name}. ${formData.message} (${formData.email})`;
     };
     return(
-        <div className='h-screen relative flex flex-col text-center md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center'>
+        <div className='min-h-screen relative flex flex-col text-center md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center'>
             <h3 className='absolute top-24 uppercase tracking-[20px] text-white text-2xl'>Contact</h3>
-            <div className='flex md:mt-32 mt-52 flex-col space-y-6'>
+            <div className='flex md:mt-32 mt-52 absolute flex-col space-y-6'>
                 <h4 className='md:text-3xl w-[400px] md:w-auto text-xl pr-16 pl-16 text-white font-semibold text-center'>
                     I have got just what you need.{" "}
                     <span className='underline decoration-[#088F8F]'>Lets Talk.</span>
@@ -40,7 +40,7 @@ export default function ContactMe() {
                     <p className='md:text-2xl'>00100, Nairobi</p>
                     </div>
                 </div>
-                <form className='md:mx-auto md:w-fit space-y-2 flex flex-col' onSubmit={handleSubmit(onSubmit)}>
+                <form className='md:mx-auto md:w-fit space-y-2 flex pb-10 flex-col' onSubmit={handleSubmit(onSubmit)}>
                     <div className='flex md:flex-row flex-col space-y-3 md:space-y-0  md:space-x-2'>
                         <input {...register('name')} placeholder='Name' className='contactInput' type="text" />
                         <input {...register('email')} placeholder='Email' className='contactInput' type="email" />
